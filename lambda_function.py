dd8819b3fea0d973b33b6a505fc7136921c41bd5
Hugo <ht0981.2005@gmail.com>
2025-05-25 00:14:32 -0300

    :books: docs: Comentários sobre o funcionamento do lambda_function

--- lambda_function.py
+++ lambda_function.py
@@ -2,18 +2,21 @@ from utils.signature_verifier import SignatureVerifier
 from commands.commands import CommandMap
 
 def lambda_handler(event, context):
-
+    # Verifica a assinatura do payload
     SignatureVerifier.verify_signature(event)
 
+    # Salva o dicionário do payload
     payload = event.get('body-json')
 
+    # Resposta para a requisição PING do discord
     if payload.get("type") == 1:
         return {
             "type": 1
         }
-
+    # Salva o mapa de comandos do bot
     commands_map = CommandMap.get_commands()
 
+    # Verifica se a classe do comando existe
     try:
         command = commands_map.get(payload["data"]["name"])(payload)
     except Exception as e:
@@ -26,6 +29,7 @@ def lambda_handler(event, context):
             }
         }
 
+    # Executa o comando
     try: 
         return command.execute()
     except Exception as e:
